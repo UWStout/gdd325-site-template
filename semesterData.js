6a8397b13c9a4b7d2d3c9e382e6b941a4c711a57
Seth Berrier <berriers@uwstout.edu>
2017-04-25 14:24:39 -0500

    Initial commit of site template files Started in Spring of 2017.

---
+++ semesterData.js
@@ -0,0 +1,10 @@
+// Customizable per-semester data
+var semesterData = {
+  studioName: 'Beige Box Studios',
+  semester: 'Fall', year: '2016',
+  GSXDate: 'December 8th, 2016',
+
+  games: [
+    { name: 'Lambent Abyss', folder: 'game' }
+  ]
+}
---
+++ splash/readme.md
@@ -0,0 +1,3 @@
+A working splash screen for the semester company should be in this directory
+setup to start from index.html and accept a URL for redirection as ?dest=[url]
+in the address.
