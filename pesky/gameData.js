0dd145e76ff6e799972c39aa1a2376111962a37c
Leslie Murphy <murles29@users.noreply.github.com>
2017-04-28 11:06:00 -0500

    updating titles, byline

--- pesky/gameData.js
+++ pesky/gameData.js
@@ -9,18 +9,18 @@ var gameData = {
   playDestination: 'play/ourLibraries/Pesky.html',
 
   /********* Please do not edit things ABOVE this line *******/
-  byline: 'Our Amazing Tagline!!',
+  byline: 'Play as three rodents and terrorize the neighborhood, watch out for animal control!',
 
   description: `
   Our gripping description.
   `,
 
   creators: [
-    { name: 'Allan Seckora', contrib: 'Artist/Animator' },
-    { name: 'Susan Evans', contrib: '[what you did]' },
+    { name: 'Allan Seckora', contrib: 'Artist / Animator' },
+    { name: 'Susan Evans', contrib: 'Artist / Original Concept Creator' },
     { name: 'Alex Hill', contrib: 'Programmer / Meme Overlord' },
-    { name: 'Dan Hying', contrib: '[what you did]' },
-    { name: 'Thomas Rosik', contrib: '[what you did]' },
+    { name: 'Dan Hying', contrib: 'Programmer / ' },
+    { name: 'Thomas Rosik', contrib: 'Programmer / ' },
     { name: 'Leslie Murphy', contrib: 'Programmer / Miss Bossy Pants' }
     
   ]
