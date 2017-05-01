dd227c3b27db7a3db53b540ee24fc551c33fe9a7
Unknown <holmank0832@my.uwstout.edu>
2017-05-01 11:45:50 -0500

    Added Description

--- memories/gameData.js
+++ memories/gameData.js
@@ -11,22 +11,25 @@ var gameData = {
   /********* Please do not edit things ABOVE this line *******/
   byline: 'Almost everyone was excited about the reservior',
 
-  description: `
-  They see me rowing, I'm crying, tearing up and seeing my heart breaking
-  tearing up and seeing my heart breaking
-  tearing up and seeing my heart breaking
-  tearing up and seeing my heart breaking
-  tearing up and seeing my heart breaking
-  My anger is loud
-  I'm defacing
-  I'm trying to fill a void to stop my heart breaking
-  trying to stop my heart breaking
-  trying to stop my heart breaking
-  trying to stop my heart breaking
-  trying to stop my heart breaking
-  WIP, WIP, WIP, WIP, WIP, WIP, WIP, WIP...
 
-  `,
+  description: '
+  Your childhood home is at the bottom of a reservoir. Solve puzzles to explore the land surrounding the lake, learn about it\'s impact on the community, and come to terms with your loss. Discover the secrets of the reservoir, inspired by historic Lake Wissota.'
+  // description: `
+  // They see me rowing, I'm crying, tearing up and seeing my heart breaking
+  // tearing up and seeing my heart breaking
+  // tearing up and seeing my heart breaking
+  // tearing up and seeing my heart breaking
+  // tearing up and seeing my heart breaking
+  // My anger is loud
+  // I'm defacing
+  // I'm trying to fill a void to stop my heart breaking
+  // trying to stop my heart breaking
+  // trying to stop my heart breaking
+  // trying to stop my heart breaking
+  // trying to stop my heart breaking
+  // WIP, WIP, WIP, WIP, WIP, WIP, WIP, WIP...
+  //
+  // `,
 
   creators: [
     { name: 'Casey Holman', contrib: 'Lead Designer, and Producer, Programmer' }, //Archmage of Game Alchemy.
