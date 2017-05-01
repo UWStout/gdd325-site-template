3525a71ce23354a2a3b128b2be532b3d539e912f
Seth Berrier <berriers@uwstout.edu>
2017-05-01 11:45:59 -0500

    Fixing File Syntax
    Removed the + and - characters

--- whitecoal/gameData.js
+++ whitecoal/gameData.js
@@ -9,26 +9,19 @@ var gameData = {
   playDestination: 'play/index.html',
 
   /********* Please do not edit things ABOVE this line *******/
--  byline: 'Our Amazing Tagline!!',
-+  byline: 'Talk to the locals and learn all about the wonders of the Lake Wissota Reservoir! ',
+  byline: 'Talk to the locals and learn all about the wonders of the Lake Wissota Reservoir! ',
  
    description: `
--  Our gripping description.
-+  A walkthrough of the 1912 Chippewa Valley region prior to Lake Wissota and the hydro-electric dam that was established between 1913 and 1918. Interact with local townsfolk for a bettern understanding of the benefits and detriments of man-made lakes. 
+  A walkthrough of the 1912 Chippewa Valley region prior to Lake Wissota and the hydro-electric dam that was established between 1913 and 1918. Interact with local townsfolk for a bettern understanding of the benefits and detriments of man-made lakes. 
    `,
  
    creators: [
--    { name: '[your name]', contrib: '[what you did]' },
--    { name: '[your name]', contrib: '[what you did]' },
--    { name: '[your name]', contrib: '[what you did]' },
--    { name: '[your name]', contrib: '[what you did]' }
-+    { name: 'Brendan Bard', contrib: 'Programmer -Lead Programer' },
-+    { name: 'Grant Hyslop', contrib: 'Artist - Lead Designer' },
-+    { name: 'Jerome "Ziggy" Ziegelbauer', contrib: 'Programmer - Lead Narrative' },
-+    { name: 'Joe King', contrib: 'Programer - Programer' },
-+    { name: 'Josh Frederick', contrib: 'Artist - Lead Art and Animation'},
-+    { name: 'Mallory Lee', contrib: 'Artist - Lead Environmental Artist' },
-+    { name: 'Rahul Shah', contrib: 'Composer and Musician' }
-+    
+    { name: 'Brendan Bard', contrib: 'Programmer -Lead Programer' },
+    { name: 'Grant Hyslop', contrib: 'Artist - Lead Designer' },
+    { name: 'Jerome "Ziggy" Ziegelbauer', contrib: 'Programmer - Lead Narrative' },
+    { name: 'Joe King', contrib: 'Programer - Programer' },
+    { name: 'Josh Frederick', contrib: 'Artist - Lead Art and Animation'},
+    { name: 'Mallory Lee', contrib: 'Artist - Lead Environmental Artist' },
+    { name: 'Rahul Shah', contrib: 'Composer and Musician' }    
    ]
 };
